ab2780e09f64fddc2a632cd26e2c627ced7b706c
Joel Huggett <joel@forestry.io>
2020-10-28 10:10:44 -0300

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export default function Home() {
+  return (
+    <>
+    
+    <Test>Hello</Test>
+    
+    </>
+  )
+}
+
+const Test = styled.div`
+  color: blue;
+`
